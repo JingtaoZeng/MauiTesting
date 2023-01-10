741331a6955692dcbe0d4186f5c872e6d40949b9
jtzeng <jtzeng41@gmail.com>
2023-01-11 00:09:07 +0800

    .NET MAUI 6 initial template

---
+++ MauiTesting/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiTesting;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiTesting/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiTesting;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
